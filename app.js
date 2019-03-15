6c30e2a6740b5661fb0d18719fab5cc73104b2ba
Mustafa Elimam <mjkam88@gmail.com>
2019-03-14 23:40:45 -0700

    Updated app.js
    Updated app.js

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-
 var express = require('express');
 var path = require('path');
-
-
-
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var viewRouter = require('./routes/view');
-var deleteRouter = require('./routes/delete');
-var addRouter = require('./routes/add');
-var aboutRouter = require('./routes/about');
-var editRouter = require('./routes/edit');
+var filmRouter = require('./routes/music');
 
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', indexRouter);
-app.use('/view', viewRouter);
-app.use('/delete', deleteRouter);
-app.use('/add', addRouter);
-app.use('/about', aboutRouter);
-app.use('/edit', editRouter);
+app.use('/api/v1/music', filmRouter);
 
-
-.use(function(req, res, next){
-
-    res.setHeader('Content-Type', 'text/plain');
-
-    res.send(404, 'SORRY!!!  PAGE NOT FOUND!');
-
-});
 
 module.exports = app;
-//app.listen(8080);
